06fba3519daf87f9d4d447c024e19ea10d7d7e7b
tifemayo <tifemayo25@gmail.com>
2025-02-07 16:48:35 +0000

    leftbar and right bar dvelepment

--- socialmediaapp/src/components/leftbar/leftbar.jsx
+++ socialmediaapp/src/components/leftbar/leftbar.jsx
@@ -1,10 +1,80 @@
 import "./leftbar.scss"
+import following from "../../assets/followers.png";
+import Community from "../../assets/community.png";
+import Analytics from "../../assets/data-analytics.png";
+import Create from "../../assets/add.png";
+import Messages from "../../assets/message.png";
+import Notifications from "../../assets/notification.png";
+// import Explore from "../../assets/search.png";
+import Videos from "../../assets/youtube.png";
+import Instagram from "../../assets/instagram.png";
+import TikTok from "../../assets/tik-tok.png";
 
+
+// import { AuthContext } from "../../context/authContext";
+// import { useContext } from "react";
 
 const LeftBar = () => {
-    return(
-        <div className="leftbar"> LeftBar </div>
-    )
-}
+    // const { currentUser } = useContext(AuthContext);
+
+    return (
+      <div className="leftBar">
+        <div className="container">
+          <div className="menu">
+            {/* <div className="user">
+              <img
+                src={currentUser.profilePic}
+                alt=""
+              />
+              <span>{currentUser.name}</span>
+            </div> */}
+            <div className="item">
+            <img src={following} alt="" />
+              <span>Following</span>
+            </div>
+            <div className="item">
+              <img src={Community} alt="" />
+              <span>Community</span>
+            </div>
+            <div className="item">
+              <img src={Analytics} alt="" />
+              <span>Analytics</span>
+            </div>
+            <div className="item">
+              <img src={Create} alt="" />
+              <span>Create</span>
+            </div>
+            <div className="item">
+              <img src={Notifications} alt="" />
+              <span>Notifications</span>
+            </div>
+            <div className="item">
+              <img src={Videos} alt="" />
+              <span>Videos</span>
+            </div>
+            <div className="item">
+              <img src={Messages} alt="" />
+              <span>Messages</span>
+            </div>
+          </div>
+          <hr />
+          <div className="menu">
+            <span>Your Platforms </span>
+            <div className="item">
+              <img src={Instagram} alt="" />
+              <span>Instagram</span>
+            </div>
+            <div className="item">
+              <img src={TikTok} alt="" />
+              <span>TikTok </span>
+            </div>
+            
+          </div>
+          <hr />
+          
+        </div>
+      </div>
+    );
+  };
 
 export default LeftBar;
--- socialmediaapp/src/components/rightbar/rightbar.jsx
+++ socialmediaapp/src/components/rightbar/rightbar.jsx
@@ -1,10 +1,203 @@
 import "./rightbar.scss"
+import friend1 from "../../images/portrait-girl-cap-with-backpack-3d-rendering.jpg";
+import friend2 from "../../images/side-view-anime-style-man-portrait.jpg";
 
 const RightBar = () => {
-    return(
-        <div className="rightbar"> RightBar </div>
-    )
-
-}
+    return (
+        <div className="rightBar">
+          <div className="container">
+            <div className="item">
+              <span>Suggestions For You</span>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src={friend1}
+                    alt=""
+                  />
+                  <span>Melissa Yam</span>
+                </div>
+                <div className="buttons">
+                  <button>follow</button>
+                  <button>dismiss</button>
+                </div>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src={friend2}
+                    alt=""
+                  />
+                  <span> Anthony Joshua</span>
+                </div>
+                <div className="buttons">
+                  <button>follow</button>
+                  <button>dismiss</button>
+                </div>
+              </div>
+            </div>
+            <div className="item">
+              <span>Latest Activities</span>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src={friend1} alt=""
+                  />
+                  <p>
+                    <span>Jane Doe</span> changed their cover picture
+                  </p>
+                </div>
+                <span>1 min ago</span>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src={friend1} alt=""
+                  />
+                  <p>
+                    <span>Jane Doe</span> changed their cover picture
+                  </p>
+                </div>
+                <span>1 min ago</span>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src={friend1} alt=""
+                  />
+                  <p>
+                    <span>Jane Doe</span> changed their cover picture
+                  </p>
+                </div>
+                <span>1 min ago</span>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src={friend1} alt=""
+                  />
+                  <p>
+                    <span>Jane Doe</span> changed their cover picture
+                  </p>
+                </div>
+                <span>1 min ago</span>
+              </div>
+            </div>
+            <div className="item">
+              <span>Online Friends</span>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="online" />
+                  <span>Jane Doe</span>
+                </div>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="online" />
+                  <span>Jane Doe</span>
+                </div>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="online" />
+                  <span>Jane Doe</span>
+                </div>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="online" />
+                  <span>Jane Doe</span>
+                </div>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="online" />
+                  <span>Jane Doe</span>
+                </div>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="online" />
+                  <span>Jane Doe</span>
+                </div>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="online" />
+                  <span>Jane Doe</span>
+                </div>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="online" />
+                  <span>Jane Doe</span>
+                </div>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="online" />
+                  <span>Jane Doe</span>
+                </div>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="online" />
+                  <span>Jane Doe</span>
+                </div>
+              </div>
+              <div className="user">
+                <div className="userInfo">
+                  <img
+                    src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="online" />
+                  <span>Jane Doe</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    };
 
 export  default RightBar;
