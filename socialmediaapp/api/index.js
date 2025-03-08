bc1b74ea5579504d393cd4adc4c2cab4b59e2b89
tifemayo <tifemayo25@gmail.com>
2025-03-08 12:24:25 +0000

    Backend Api Setup of Login functionality

--- socialmediaapp/api/index.js
+++ socialmediaapp/api/index.js
@@ -7,9 +7,14 @@ import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
 import userRoutes from "./routes/users.js"; 
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 //middleware 
 app.use(express.json())
+app.use(cors());
+app.use(cookieParser());
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
