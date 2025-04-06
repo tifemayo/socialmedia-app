476d11745be4bea0b4a5950f00e6bd9634b131a2
tifemayo <tifemayo25@gmail.com>
2025-04-06 13:12:01 +0100

    Post page filter by functionality

--- socialmediaapp/src/components/posts/Posts.jsx
+++ socialmediaapp/src/components/posts/Posts.jsx
@@ -4,6 +4,11 @@ import { useQuery } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
 
 const Posts = () => {
+
+  //Functionality to filter posts by platforms 
+  const [selectedPlatforms, setSelectedPlatforms] = useState([])
+  
+  //query to pull the data (posts) from backend 
   const { isLoading, error, data } = useQuery({
     queryKey: ["posts"],
     queryFn: async () => {
@@ -14,14 +19,43 @@ const Posts = () => {
     }
   });
 
+  //filter functionality  continues
+  const [filteredPosts, setFilteredPosts] = useState( Posts)
+  const platforms = ["instagram", "tiktok", "unifeed"];
+
+  const handlePlatformClick = (selectedCategory) => {
+    if (selectedPlatforms.includes(selectedCategory)) {
+      const platforms= selectedPlatforms.filter((el) => el !== selectedCategory);
+      setSelectedPlatforms(platforms);
+    } else {
+      setSelectedPlatforms([...selectedPlatforms, selectedCategory]);
+    }
+  };
+  useEffect(() => {
+    filterPlatforms();
+  }, [selectedPlatforms]);
+
+  const filterPlatforms =() => {
+    
+  }
+
   return (
-    <div className="posts">
-      {error
-        ? "Something went wrong!"
-        : isLoading
-        ? "loading"
-        : data.map((posts) => <Post post={posts} key={posts.id} />)}
+    <div className="postcontainer">
+      <div className="platformFilter">
+
+      </div>
+
+    
+      <div className="posts">
+        
+        {error
+          ? "Something went wrong!"
+          : isLoading
+          ? "loading"
+          : data.map((posts) => <Post post={posts} key={posts.id} />)}
+      </div>
     </div>
+  
   );
 };
 
