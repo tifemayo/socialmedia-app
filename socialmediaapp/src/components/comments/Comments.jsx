58074e2c5ffb14ff9fbb82d888c6d3766da48e11
tifemayo <tifemayo25@gmail.com>
2025-04-13 00:36:53 +0100

    Bug fix comment profile pic display

--- socialmediaapp/src/components/comments/Comments.jsx
+++ socialmediaapp/src/components/comments/Comments.jsx
@@ -1,54 +1,54 @@
 import { useContext, useState  } from "react";
 import "./comments.scss";
 import { AuthContext } from "../../context/authContext";
-import {  useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
 import moment from "moment";
 
 const Comments = ({ postId }) => {
- 
-
-
   const [desc, setDesc] = useState("");
   const { currentUser } = useContext(AuthContext);
+  const queryClient = useQueryClient();
 
   const { isLoading, error, data } = useQuery({
     queryKey: ["comments", postId],
-    queryFn: () => makeRequest.get(`/comments?postId=${postId}`).then(res => res.data),
+    queryFn: () => makeRequest.get(`/comments?postId=${postId}`).then(res => res.data)
   });
-
-  const queryClient = useQueryClient();
 
   const mutation = useMutation({
-    mutationFn: (newComment) => {
-      return makeRequest.post("/comments", newComment);
-    },
-      onSuccess: () => {
-        // Invalidate and refetch
-        queryClient.invalidateQueries(["comments"]);
-      },
+    mutationFn: (newComment) => makeRequest.post("/comments", newComment),
+    onSuccess: () => queryClient.invalidateQueries(["comments"])
   });
 
-  const handleClick = async (e) => {
+  const handleClick = (e) => {
     e.preventDefault();
     mutation.mutate({ desc, postId });
     setDesc("");
   };
 
+  const getImageUrl = (imagePath) => {
+    if (!imagePath) return "/images/default-profile.jpg";
+    if (imagePath.startsWith('http') || imagePath.startsWith('/')) {
+      return imagePath;
+    }
+    return "/upload/" + imagePath;
+  };
+
   return (
     <div className="comments">
       <div className="write">
-        <img src={currentUser.profilePic} alt="" />
-        <input type="text" placeholder="write a comment" value={desc} onChange={(e) => setDesc(e.target.value)}/>
+        <img src={getImageUrl(currentUser?.profilePic)} alt="" />
+        <input 
+          type="text" 
+          placeholder="write a comment" 
+          value={desc} 
+          onChange={(e) => setDesc(e.target.value)}
+        />
         <button onClick={handleClick}>Send</button>
       </div>
-      {error
-        ? "Something went wrong"
-        : isLoading
-        ? "loading"
-        : data.map((comment) =>(
-        <div className="comment">
-          <img src={comment.profilePic} alt="" />
+      {error ? "Something went wrong" : isLoading ? "loading" : data.map((comment) => (
+        <div className="comment" key={comment.id}>
+          <img src={getImageUrl(comment.profilePic)} alt="" />
           <div className="info">
             <span>{comment.name}</span>
             <p>{comment.desc}</p>
