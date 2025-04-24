5c5550e417669912466f5c00798f925105525e8f
tifemayo <tifemayo25@gmail.com>
2025-04-25 00:13:51 +0100

    Logout routes added in API

--- socialmediaapp/api/routes/auth.js
+++ socialmediaapp/api/routes/auth.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.post("/login", login)
 router.post("/register", register)
-router.post("/logout", logout)
+router.post("/logout", logout);
 
 
 export default router
