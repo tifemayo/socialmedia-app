5eb6eab95af1a89492860fa0178c12a047d87bbb
tifemayo <tifemayo25@gmail.com>
2025-04-29 19:55:26 +0100

    Timer functionality

---
+++ socialmediaapp/src/components/timer/Timer.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+
+const Timer = ({ timeLimit, onLimitReached }) => {
+    const [activeTime, setActiveTime] = useState(0);
+    const [isActive, setIsActive] = useState(true);
+
+    useEffect(() => {
+        let timer;
+        if (isActive) {
+            timer = setInterval(() => {
+                setActiveTime(prevTime => prevTime + 1);
+            }, 1000); // Increment every second
+        }
+
+        return () => clearInterval(timer);
+    }, [isActive]);
+
+    useEffect(() => {
+        if (activeTime >= timeLimit) {
+            setIsActive(false);
+            onLimitReached();
+        }
+    }, [activeTime, timeLimit, onLimitReached]);
+
+    return (
+        <div>
+            <h2>Active Time: {activeTime} seconds</h2>
+        </div>
+    );
+};
+
+export default Timer;
